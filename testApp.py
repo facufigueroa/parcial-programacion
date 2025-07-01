ee67ed1cd233035b76df8607ce5e0215e36482b8
facufigueroa <facundoleonelfigueroa@hotmail.com>
2025-07-01 11:28:20 -0300

    Agregué test de constructores

--- testApp.py
+++ testApp.py
@@ -1,10 +1,45 @@
 import unittest
-from app import LibraryItem, Book, Magazine, loadLibraryItems, checkoutItems, countItems, findByTitle
+from app import Book, Magazine, loadLibraryItems, checkoutItems, countItems, findByTitle
 
 class TestApp(unittest.TestCase) :
-    # def testCasoPositivo(self) :
-    #     book = Book("El principito", 15, "Juan Perez", 153)
-    #     self.assertEqual(book, )
+    def testBookConstructorPositive(self) :
+        book = Book("El principito", 15, "Juan Perez", 153)
+
+        self.assertEqual(book.title, "El principito")
+        self.assertEqual(book.itemId, 15)
+        self.assertEqual(book.author, "Juan Perez")
+        self.assertEqual(book.pages, 153)
+
+    def testBookConstructorNegative(self) :
+        with self.assertRaises(TypeError) :
+            Book(1526, 15, "Juan Perez", 153)
+            Book("El principito", "15", "Juan Perez", 153)
+            Book("El principito", 15, 159466, 153)
+            Book("El principito", 15, "Juan Perez", "153")
+        
+        with self.assertRaises(ValueError) :
+            Book("", 15, "Juan Perez", 153)
+            Book("El principito", 0, "Juan Perez", 153)
+            Book("El principito", 15, "", 153)
+            Book("El principito", 15, "Juan Perez", -1)
+    
+    def testMagazineConstructorPositive(self) :
+        magazine = Magazine("Las aventuras de Juan", 15, 152)
+
+        self.assertEqual(magazine.title, "Las aventuras de Juan")
+        self.assertEqual(magazine.itemId, 15)
+        self.assertEqual(magazine.issueNumber, 152)
+
+    def testMagazineConstructorNegative(self) :
+        with self.assertRaises(TypeError) :
+            Magazine(1565, 15, 152)
+            Magazine("Las aventuras de Juan", "15", 152)
+            Magazine("Las aventuras de Juan", 15, "152")
+        
+        with self.assertRaises(ValueError) :
+            Magazine("", 15, 152)
+            Magazine("Las aventuras de Juan", 0, 152)
+            Magazine("Las aventuras de Juan", 15, -1)
 
     def testCheckoutPositive(self) :
         book = Book("El principito", 15, "Juan Perez", 153)
