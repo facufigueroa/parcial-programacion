15d976d939a779f574284702ca252ad20a5e42c0
facufigueroa <facundoleonelfigueroa@hotmail.com>
2025-07-03 17:04:06 -0300

    Agregué comentarios a los test

--- testClasses.py
+++ testClasses.py
@@ -2,6 +2,8 @@ import unittest
 from classes import Book, Magazine
 
 class TestClasses(unittest.TestCase) :
+
+    # Test positivo del constructor de la clase Book.
     def testBookConstructorPositive(self) :
         book = Book("El principito", 15, "Juan Perez", 153)
 
@@ -10,31 +12,42 @@ class TestClasses(unittest.TestCase) :
         self.assertEqual(book.author, "Juan Perez")
         self.assertEqual(book.pages, 153)
     
+    # Test negativo del constructor de la clase Book.
     def testBookConstructorNegative(self) :
+
+        # Title recibe int en lugar de str.
         with self.assertRaises(TypeError) :
             Book(1526, 15, "Juan Perez", 153)
         
+        # ItemId recibe str en lugar de int.
         with self.assertRaises(TypeError) :
             Book("El principito", "15", "Juan Perez", 153)
         
+        # Author recibe int en lugar de str.
         with self.assertRaises(TypeError) :
             Book("El principito", 15, 159466, 153)
         
+        # Pages recibe str en lugar de int.
         with self.assertRaises(TypeError) :
             Book("El principito", 15, "Juan Perez", "153")
         
+        # Title recibe una cadena vacía.
         with self.assertRaises(ValueError) :
             Book("", 15, "Juan Perez", 153)
         
+        # ItemId recibe un número menor o igual a cero.
         with self.assertRaises(ValueError) :
             Book("El principito", 0, "Juan Perez", 153)
         
+        # Author recibe una cadena vacía.
         with self.assertRaises(ValueError) :
             Book("El principito", 15, "", 153)
         
+        # Pages recibe un número menor o igual a cero.
         with self.assertRaises(ValueError) :
             Book("El principito", 15, "Juan Perez", -1)
     
+    # Test positivo del constructor de la clase Magazine.
     def testMagazineConstructorPositive(self) :
         magazine = Magazine("Las aventuras de Juan", 15, 152)
 
@@ -42,38 +55,54 @@ class TestClasses(unittest.TestCase) :
         self.assertEqual(magazine.itemId, 15)
         self.assertEqual(magazine.issueNumber, 152)
     
+    # Test negativo del constructor de la clase Magazine.
     def testMagazineConstructorNegative(self) :
+
+        # Title recibe int en lugar de str.
         with self.assertRaises(TypeError) :
             Magazine(1565, 15, 152)
         
+        # ItemId recibe str en lugar de int.
         with self.assertRaises(TypeError) :
             Magazine("Las aventuras de Juan", "15", 152)
         
+        # IssueNumber recibe str en lugar de int.
         with self.assertRaises(TypeError) :
             Magazine("Las aventuras de Juan", 15, "152")
         
+        # Title recibe una cadena vacía.
         with self.assertRaises(ValueError) :
             Magazine("", 15, 152)
         
+        # ItemId recibe un número menor o igual a cero.
         with self.assertRaises(ValueError) :
             Magazine("Las aventuras de Juan", 0, 152)
         
+        # IssueNumber recibe un número menor o igual a cero.
         with self.assertRaises(ValueError) :
             Magazine("Las aventuras de Juan", 15, -1)
     
+    # Test positivo del método checkout de las clases Book y Magazine.
     def testCheckoutPositive(self) :
         book = Book("El principito", 15, "Juan Perez", 153)
         magazine = Magazine("Las aventuras de Juan", 15, 152)
 
-        self.assertEqual(book.checkout("Facu"), "Book 'El principito' checked out by Facu.")
-        self.assertEqual(magazine.checkout("Facu"), "Magazine 'Las aventuras de Juan' issue 152 checked out by Facu.")
+        bookExpected = "Book 'El principito' checked out by Facu."
+        magazineExpected = "Magazine 'Las aventuras de Juan' issue 152 checked out by Facu."
+
+        self.assertEqual(book.checkout("Facu"), bookExpected)
+        self.assertEqual(magazine.checkout("Facu"), magazineExpected)
     
+    # Test negativo del método checkout de las clases Book y Magazine.
     def testCheckoutNegative(self) :
         book = Book("El principito", 15, "Juan Perez", 153)
         magazine = Magazine("Las aventuras de Juan", 15, 152)
 
-        self.assertNotEqual(book.checkout("Facu"), "Book 'El principito' checked out by .")
-        self.assertNotEqual(magazine.checkout("Facu"), "Magazine 'Las aventuras de Juan' issue 152 checked out by .")
+        bookNotExpected = "Book 'El principito' checked out by ."
+        magazineNotExpected = "Magazine 'Las aventuras de Juan' issue 152 checked out by ."
+
+        self.assertNotEqual(book.checkout("Facu"), bookNotExpected)
+        self.assertNotEqual(magazine.checkout("Facu"), magazineNotExpected)
 
 if __name__ == "__main__" :
     unittest.main()
--- testUtils.py
+++ testUtils.py
@@ -5,6 +5,8 @@ from classes import Book, Magazine
 from utils import loadLibraryItems, checkoutItems, countItems, findByTitle
 
 class TestUtils(unittest.TestCase) :
+
+    # Test positivo de la función loadLibraryItems.
     def testLoadLibraryItemsPositive(self) :
         with tempfile.NamedTemporaryFile(mode="w+", suffix=".csv", delete=False, newline="") as tempCsvFile :
             csvWriter = csv.writer(tempCsvFile)
@@ -16,6 +18,7 @@ class TestUtils(unittest.TestCase) :
 
         self.assertEqual(items, expected)
 
+    # Test negativo de la función loadLibraryItems.
     def testLoadLibraryItemsNegative(self) :
         with tempfile.NamedTemporaryFile(mode="w+", suffix=".csv", delete=False) as tempCsvFile :
             csvWriter = csv.writer(tempCsvFile)
@@ -23,21 +26,28 @@ class TestUtils(unittest.TestCase) :
             csvWriter.writerow(["book", 15, "Juan Perez", 153])
             csvWriter.writerow(["magazine", "Las aventuras de Juan", 152])
 
+        # La función recibe menos datos de los que necesita.
         with self.assertRaises(ValueError) :
             loadLibraryItems(tempCsvFile.name)         
     
+    # Test positivo de la función checkoutItems.
     def testCheackoutItemsPositive(self) :
         libraryItems = [Book("El principito", 15, "Juan Perez", 153), Magazine("Las aventuras de Juan", 15, 152)]
         checkoutLibraryItems = checkoutItems(libraryItems, "Facu")
 
-        self.assertEqual(checkoutLibraryItems, ["Book 'El principito' checked out by Facu.", "Magazine 'Las aventuras de Juan' issue 152 checked out by Facu."])
+        expected = ["Book 'El principito' checked out by Facu.", "Magazine 'Las aventuras de Juan' issue 152 checked out by Facu."]
 
+        self.assertEqual(checkoutLibraryItems, expected)
+
+    # Test negativo de la función checkoutItems.
     def testCheackoutItemsNegative(self) :
         libraryItems = [Book("El principito", 15, "Juan Perez", 153), Magazine("Las aventuras de Juan", 15, 152)]
 
+        # El parámetro user de la función es de tipo str y recibe int.
         with self.assertRaises(TypeError) :
             checkoutItems(libraryItems, 5)
     
+    # Test positivo de la función countItems.
     def testCountItemsPositive(self) :
         libraryItems = [Book("El principito", 15, "Juan Perez", 153), Magazine("Las aventuras de Juan", 15, 152)]
         count = {
@@ -47,6 +57,7 @@ class TestUtils(unittest.TestCase) :
 
         self.assertEqual(countItems(libraryItems), count)
 
+    # Test negativo de la función countItems.
     def testCountItemsNegative(self) :
         libraryItems = [Book("El principito", 15, "Juan Perez", 153), Magazine("Las aventuras de Juan", 15, 152)]
         count = {
@@ -56,6 +67,7 @@ class TestUtils(unittest.TestCase) :
 
         self.assertNotEqual(countItems(libraryItems), count)
 
+    # Test positivo de la función findByTitle.
     def testFindByTitlePositive(self) :
         libraryItems = [Book("El principito", 15, "Juan Perez", 153), Magazine("Las aventuras de Juan", 15, 152)]
         itemsByTitle1 = findByTitle(libraryItems, "Principito")
@@ -64,9 +76,11 @@ class TestUtils(unittest.TestCase) :
         self.assertEqual(itemsByTitle1[0], libraryItems[0])
         self.assertEqual(itemsByTitle2, [])
 
+    # Test negativo de la función findByTitle.
     def testFindByTitleNegative(self) :
         libraryItems = [Book("El principito", 15, "Juan Perez", 153), Magazine("Las aventuras de Juan", 15, 152)]
 
+        # El parámetro keyword de la función es de tipo str y recibe int.
         with self.assertRaises(TypeError) :
             findByTitle(libraryItems, 56)
 
